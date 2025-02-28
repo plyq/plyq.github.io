729ec643a387c615a858e8210c07e812d1d2bcbb
Artem <xanatos.shubunkin@gmail.com>
2025-02-28 12:55:37 +0100

    fixes

--- assets/js/mem_battle.js
+++ assets/js/mem_battle.js
@@ -3,7 +3,7 @@ const mem1 = document.getElementById('mem1');
 const mem2 = document.getElementById('mem2');
 const memwinner = document.getElementById('memwinner');
 const mheader = document.getElementById('mheader');
-let candidates = document.getElementById('memgen').getAttribute('imgsdata').split(",");
+let candidates = document.getElementById('memgen').getAttribute('imgsdata').split(",").slice(0, -1);
 
 const context1 = mem1.getContext('2d');
 const context2 = mem2.getContext('2d');
@@ -27,7 +27,7 @@ const showImages = () => {
         img.src = candidates[0];
         img.onload = () => {
             const aspectRatio = img.height / img.width;
-            memwinner.width = window.innerWidth * 0.4; // Set canvas width to 80% of viewport width
+            memwinner.width = Math.min(window.innerWidth * 0.4, img.width);
             memwinner.height = memwinner.width * aspectRatio;
             contextWinner.drawImage(img, 0, 0, memwinner.width, memwinner.height);
         };
@@ -40,16 +40,17 @@ const showImages = () => {
     const img2 = new Image();
     img1.src = candidates[index1];
     img2.src = candidates[index2];
+
     img1.onload = () => {
         const aspectRatio1 = img1.height / img1.width;
-        mem1.height = window.innerHeight * 0.8; // Set canvas height to 80% of viewport height
-        mem1.width = mem1.height / aspectRatio1;
+        mem1.width = Math.min(window.innerWidth * 0.4, img1.width);
+        mem1.height = mem1.width * aspectRatio1;
         context1.drawImage(img1, 0, 0, mem1.width, mem1.height);
     };
     img2.onload = () => {
         const aspectRatio2 = img2.height / img2.width;
-        mem2.height = window.innerHeight * 0.8; // Set canvas height to 80% of viewport height
-        mem2.width = mem2.height / aspectRatio2;
+        mem2.width = Math.min(window.innerWidth * 0.4, img2.width);
+        mem2.height = mem2.width * aspectRatio2;
         context2.drawImage(img2, 0, 0, mem2.width, mem2.height);
     };
 
